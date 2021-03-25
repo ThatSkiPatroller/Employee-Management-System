01438f21c7ab3301d7e64176ba73a1b2687d3850
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-03-24 23:38:20 -0500

    Created more inquirer prompts and added more content to database functions

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const consoletable = require('console.table');
 // const sequelize = require('./Config/connections');
 // Do we need sequelize?
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Tdragon9!',
+    database: 'employees_db'
+});
 
-// connection.connect((err) => {
-//     if (err) throw err;
-//     start();
-// });
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+});
 
 
 const start = () => {
@@ -88,19 +88,83 @@ const byMngr = () => {
 };
 
 const addEmployee = () => {
-    console.log('this is addEmployee');
+    inquirer
+        .prompt({
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "role",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: [
+            "Sales lead", "Salesperson", "Lead Engineer", "Software Engineer", 
+            "Account Manager", "Accountant", "Legal Team Lead"
+        ]
+        },
+        {
+            name: "employeeMngr",
+            type: "list",
+            message: "Who is the emplyee's manager?",
+            // Need to get managers from employees_db for choices
+            choices: []
+        })
 };
 
-const remEmployee = () => {
-    console.log('this is remEmployee');
+// Get employees 
+const getEmployees = () => {
+    return new Promise((res, rej) => {
+        connection.query(`SELECT CONCAT ()`)
+    })
+}
+const remEmployee = async () => {
+    let choices = await employees_db.getEmployees();
+    console.log(choices);
+    inquirer
+        .prompt({
+            name: "removeEmployee",
+            type: "list",
+            message: "Which employee do you want to remove?",
+            choices: choices
+        })
+        .then((answer) => {
+            const query = 'SELECT first_name, last_name FROM employees_db';
+            connection.query(query, (err, res) => {
+                res.forEach(({ first_name, last_name }) => {
+                    console.table([{first_name, last_name}])
+                });
+                if (err) throw err;
+            });
+        });
 };
 
 const upEmployee = () => {
+    inquirer
+        .prompt({
+            name: "updateEmployee",
+            type: "list",
+            message: "Which employee do you want to update?",
+            // Need to get employees for choices
+            choices: []
+        })
     console.log('this is upEmployee');
 };
 
 const upMngr = () => {
+    inquirer
+    .prompt({
+        name: "updateManager",
+        type: "list",
+        message: "Which manager do you want to update?",
+        // Need to get employees for choices
+        choices: []
+    })
     console.log('this is upMngr');
 };
 
-start();
